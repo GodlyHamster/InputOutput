5144340ea03da4e1872dae4f7ff83d6c9340ee30
GodlyHamster <40576028+GodlyHamster@users.noreply.github.com>
2021-03-19 16:42:25 +0100

    Hand movement options

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Leap;
@@ -6,31 +7,67 @@ using Leap;
 public class HandMovement : MonoBehaviour
 {
 
+    enum ControlSelection
+    {
+        Leap,
+        Keys
+    };
+
+    [SerializeField] ControlSelection Controller = ControlSelection.Leap;
+
     [SerializeField] private GameObject[] _Hands;
 
     Controller controller;
 
-    // Start is called before the first frame update
     void Start()
     {
         controller = new Controller();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if (Controller == ControlSelection.Leap)
+        {
+            UsingLeap();
+        }else if(Controller == ControlSelection.Keys)
+        {
+            UsingKeys();
+        }
+    }
+
+    void UsingKeys()
+    {
+        float rH = Input.GetAxis("RightHand");
+        float lH = Input.GetAxis("LeftHand");
+
+        KeySetHandPos(0, lH);
+        KeySetHandPos(1, rH);
+    }
+    void KeySetHandPos(int hand, float height)
+    {
+        float xOffset = 0;
+
+        if (hand == 0) { xOffset = -3; }
+        else if (hand == 1) { xOffset = 3; }
+
+        _Hands[hand].transform.position = new Vector3(xOffset, height * 3, 0);
+    }
+
+    void UsingLeap()
     {
         List<Hand> hands;
         Hand leftHand = null;
         Hand rightHand = null;
 
-        Frame frame = controller.Frame(); // controller is a Controller object
+        Frame frame = controller.Frame();
         if (frame.Hands.Count > 0)
         {
             hands = frame.Hands;
             if (hands[0].IsLeft)
             {
                 leftHand = hands[0];
-            }else if (hands[0].IsRight)
+            }
+            else if (hands[0].IsRight)
             {
                 rightHand = hands[0];
             }
@@ -46,48 +83,45 @@ public class HandMovement : MonoBehaviour
                     rightHand = hands[1];
                 }
             }
-            
-            
+
             if (leftHand != null)
             {
-                CheckHandPos(leftHand);
+                LeapSetHandPos(leftHand);
             }
             if (rightHand != null)
             {
-                CheckHandPos(rightHand);
+                LeapSetHandPos(rightHand);
             }
-
         }
-        
+    }
 
-        void CheckHandPos(Hand h)
+    void LeapSetHandPos(Hand h)
+    {
+        int handUsed = 0;
+        float offset = 0;
+
+        if (h.IsLeft)
         {
-            int handUsed = 0;
-            float offset = 0;
-
-            if (h.IsLeft)
-            {
-                handUsed = 0;
-                offset = -3;
-            }else if (h.IsRight)
-            {
-                handUsed = 1;
-                offset = 3;
-            }
-
-            if (h.PalmPosition.z <= -50)
-            {
-                _Hands[handUsed].transform.position = new Vector3(offset, 3, 0);
-            }
-            else if (h.PalmPosition.z >= 50)
-            {
-                _Hands[handUsed].transform.position = new Vector3(offset, -3, 0);
-            }
-            else
-            {
-                _Hands[handUsed].transform.position = new Vector3(offset, 0, 0);
-            }
+            handUsed = 0;
+            offset = -3;
+        }
+        else if (h.IsRight)
+        {
+            handUsed = 1;
+            offset = 3;
         }
 
+        if (h.PalmPosition.z <= -50)
+        {
+            _Hands[handUsed].transform.position = new Vector3(offset, 3, 0);
+        }
+        else if (h.PalmPosition.z >= 50)
+        {
+            _Hands[handUsed].transform.position = new Vector3(offset, -3, 0);
+        }
+        else
+        {
+            _Hands[handUsed].transform.position = new Vector3(offset, 0, 0);
+        }
     }
 }
