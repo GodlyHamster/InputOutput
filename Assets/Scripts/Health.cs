2f4485c42c5e4dd48a1391a2f26b6baa9e993228
Ruben Dudink <rubendudink2003@gmail.com>
2021-03-22 15:53:00 +0100

    health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cube")
+        {
+
+            TakeDamage(20);
+        }
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("currenHealth");
+    }
+   
+}
